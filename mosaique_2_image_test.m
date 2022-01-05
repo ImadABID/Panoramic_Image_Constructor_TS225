2273d15bab3155232326960b0eefc76c63327d86
ImadAbid <imad.abied@gmail.com>
2022-01-05 15:46:29 +0100

    implementing n image test script

---
+++ mosaique_2_image_test.m
@@ -0,0 +1,49 @@
+clear all;
+close all;
+clc;
+
+origine_path = "our_images/petites_imgs/img01.jpg.jpeg";
+target_path = "our_images/petites_imgs/img02.jpg.jpeg";
+
+origine = imread(origine_path);
+target = imread(target_path);
+
+origine = rgb2gray(origine);
+target = rgb2gray(target);
+
+% figure(1),
+% subplot(1,2,1);
+% imshow(origine);
+% subplot(1,2,2);
+% imshow(target);
+% title("test");
+
+figure,
+imshow(origine);
+pts_o = ginput(4);
+title(origine_path);
+
+figure,
+imshow(target);
+pts_t = ginput(4);
+title(target_path);
+
+H_for_boite = homographic_matrix(pts_t, pts_o);
+H_for_browse = homographic_matrix(pts_o, pts_t);
+
+
+mosaique_o = image_to_mosaique(origine);
+mosaique_t = image_to_mosaique(target);
+
+mosaique_transform  = transform_mosaique(mosaique_t, H_for_boite, H_for_browse);
+
+figure,
+imshow(uint8(mosaique_transform.image));
+title("output_tr");
+
+new_mosaique = merge_two_mosaique(mosaique_o,mosaique_transform);
+
+figure,
+imshow(uint8(new_mosaique.image));
+title("output");
+
---
+++ mosaique_n_image_test.m
@@ -0,0 +1,48 @@
+clear all;
+close all;
+clc;
+
+images_path = [ ...
+    "our_images/petites_imgs/img01.jpg.jpeg",   ...
+    "our_images/petites_imgs/img02.jpg.jpeg",   ...
+    "our_images/petites_imgs/img03.jpg.jpeg"    ...
+];
+
+images_nbr = length(images_path);
+
+image = rgb2gray(imread(images_path(1)));
+assembled_mosaique = image_to_mosaique(image);
+
+mosaique_figure = figure;
+ginput_figure = figure;
+
+figure(mosaique_figure),
+imshow(uint8(assembled_mosaique.image));
+title('Mosaique');
+
+for i=2:1:images_nbr
+
+    image = rgb2gray(imread(images_path(i)));
+    tmp_mosaique = image_to_mosaique(image);
+    figure(ginput_figure),
+    imshow(uint8(tmp_mosaique.image));
+    title('image to insert : Select 4 points !!');
+    pts_t = ginput(4);
+    title('image to insert');
+    
+    figure(mosaique_figure),
+    title('Mosique : Select 4 points !!');
+    pts_o = ginput(4);
+    title('Mosique');
+
+    H_for_boite = homographic_matrix(pts_t, pts_o);
+    H_for_browse = homographic_matrix(pts_o, pts_t);
+
+    tmp_mosaique  = transform_mosaique(tmp_mosaique, H_for_boite, H_for_browse);
+
+    assembled_mosaique = merge_two_mosaique(tmp_mosaique, assembled_mosaique);
+    
+    figure(mosaique_figure), imshow(uint8(assembled_mosaique.image));
+
+end
+
